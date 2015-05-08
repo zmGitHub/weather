e36fc9d586cba32d30f272aa9b990151746d8444
turbo <691209942@qq.com>
2015-05-08 09:33:37 +0800

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,74 @@
+angular.module('weather', ['ionic', 'weather.controllers'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+        $ionicConfigProvider.platform.ios.tabs.style('standard');
+        $ionicConfigProvider.platform.ios.tabs.position('bottom');
+        $ionicConfigProvider.platform.android.tabs.style('standard');
+        $ionicConfigProvider.platform.android.tabs.position('standard');
+        $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+        $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+        $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+        $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-ios-arrow-back');
+        $ionicConfigProvider.platform.ios.views.transition('ios');
+        $ionicConfigProvider.platform.android.views.transition('android');
+        $stateProvider
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.weather', {
+                url: "/weather",
+                views: {
+                    'tab-weather': {
+                        templateUrl: "templates/weather.html"
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'app-browse': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'app-playlists': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'app-single': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/weather');
+    });
